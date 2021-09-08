19eacb3f61719e8dc8d48323ea68d814944497a5
xval <valentinocarneirogoncalves@gmail.com>
2021-09-08 14:32:37 -0700

    Begging of the chaimager_adder screen

---
+++ AbsolutReader/src/js/chaimager_adder.js
@@ -0,0 +1,34 @@
+import React, {Component} from 'react';
+import * as eva from '@eva-design/eva';
+import { ApplicationProvider, Layout, Divider, Button, TopNavigation, Icon,Text, TopNavigationAction, List, Card, Modal} from '@ui-kitten/components';
+import { Image, StyleSheet, SafeAreaView, Dimensions, View} from 'react-native';
+import DocumentPicker from 'react-native-document-picker';
+import ColorPicker from 'react-native-wheel-color-picker'
+import DocumentPicker from 'react-native-document-picker';
+
+var RNFS = require('react-native-fs');
+
+export default class Chaimager_adder extends Component {
+    //User screen to create a new chaimager file
+
+    constructor(props){
+        super(props);
+
+        this.state = {
+            function: props.route.params["name"] //can be editing or creating new chaimager
+        }
+    }
+
+
+
+    render () {
+        return (
+            <SafeAreaView style={{ flex: 1 }}>
+
+
+
+            </SafeAreaView>
+        )
+    }
+
+}
