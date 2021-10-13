7bb67e4355144a91642e60ad9e056736364f0269
xval <valentinocarneirogoncalves@gmail.com>
2021-10-13 12:41:13 -0700

    Share of Chaimager files

---
+++ AbsolutReader/src/js/chaimager_sharing/exporter.js
@@ -0,0 +1,7 @@
+
+
+function create_package (chaimager_file) {
+
+    //chaimager_file is a .json file, so we zip it and name it with a diferent
+    //File extension
+}
---
+++ AbsolutReader/src/js/chaimager_sharing/importer.js
@@ -0,0 +1,15 @@
+function unpack (file) {
+
+    //Converts the .arcm file (which is a zip file) to .json to be read and 
+    //added to the library
+
+}
+
+export async function _import (file) {
+    //Importing a Chaimager file
+
+    var unpacked_file = await unpack(file);
+
+    //Adding to library
+
+}
